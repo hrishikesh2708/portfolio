84629ece2287b284295f3fcb38668f410a12b80b
Hrishikesh2708 <hrishith27@gmail.com>
2023-10-10 23:18:34 -0700

    Initial stack commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="bg-blue-500">
+          <h1>This is a Tailwind React app!</h1>
+        </div>
+      </header>
     </div>
   );
 }
