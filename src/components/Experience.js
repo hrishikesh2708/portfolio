4a21d7566f7978446f5874ad64160572a1c32a91
Hrishikesh2708 <hrishith27@gmail.com>
2023-10-13 13:39:57 -0700

    updated experience styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -44,7 +44,14 @@ function Experience() {
       role: "Software Engineer Co-op",
       attachments: "",
       project: [
-        { title: "Data Catalog", discription: "" },
+        {
+          title: "Data Catalog",
+          discription: `Developed data catalog product to provide a centralized data ingestion framework for applications to
+        consume without having to build and maintain. leveraged spring boot and hibernate for building backend APIs. Generated
+        flow to ingest large volumes of data from ERPs.Also, computerized various data ingestion steps such as file processing, and
+        historical and incremental data updates thereby cutting down significant time required to manually transfer and supervise
+        files/data`,
+        },
         {
           title: " B2B Invoice Management Application",
           discription: ` Created a full-stack web-based product for handling invoices, and obtained a
@@ -92,31 +99,108 @@ function Experience() {
       <h4 class="text-md font-bold dark:text-white p-1 m-2 underline underline-offset-8">
         Experience
       </h4>
-      <div>
-        {content.map((element) => {
-          return (
-            <div>
-              <p>{element.company}</p>
-              <p>
-                {element.startDate}
-                {` ~ `}
-                {element.endDateD}
+      <section>
+        <div class="bg-black text-white py-8">
+          <div class="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
+            <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
+              <p class="ml-2 text-yellow-300 uppercase tracking-loose">
+                Working Process
               </p>
-              <p>{element.role}</p>
-              <p>{element.location}</p>
-              {element?.project.map((ele) => {
-                return (
-                  <div>
-                    <p>{ele?.title}</p>
-                    <p>{ele?.discription}</p>
-                    <p>{ele?.attachments}</p>
-                  </div>
-                );
-              })}
+              <p class="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">
+                Working Process of Fest
+              </p>
+              <p class="text-sm md:text-base text-gray-50 mb-4">
+                Here’s your guide to the tech fest 2021 process. Go through all
+                the steps to know the exact process of the fest.
+              </p>
+              <a
+                href="#"
+                class="bg-transparent mr-auto hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
+              >
+                Explore Now
+              </a>
             </div>
-          );
-        })}
-      </div>
+            <div class="ml-0 md:ml-12 lg:w-2/3 sticky">
+              <div class="container mx-auto w-full h-full">
+                <div class="relative wrap overflow-hidden p-10 h-full">
+                  <div
+                    class="border-2-2 border-yellow-555 absolute h-full border"
+                    // style="right: 50%; border: 2px solid #FFC100; border-radius: 1%;"
+                  ></div>
+                  <div
+                    class="border-2-2 border-yellow-555 absolute h-full border"
+                    // style="left: 50%; border: 2px solid #FFC100; border-radius: 1%;"
+                  ></div>
+                  {content.map((element, index) => {
+                    return index % 2 === 0 ? (
+                      <>
+                        <div class="mb-8 flex justify-between items-center w-full right-timeline text-shade-1">
+                          <div class="order-1 w-5/12"></div>
+                          <div class="order-1  w-5/12 px-1 py-4 text-left">
+                            <p class="mb-2 text-sm text-accent-2">
+                              {element.startDate}
+                              {`~`}
+                              {element?.endDate}
+                            </p>
+                            <h4 class="mb-3 font-bold text-md md:text-2xl">
+                              {element?.company}
+                            </h4>
+                            <h4 class="mb-1 italic text-xs md:text-2xl">
+                              {element?.role}
+                            </h4>
+                            {element?.project.map((ele) => {
+                              return (
+                                <p class="text-xs md:text-base leading-snug text-shade-1 text-opacity-100">
+                                  {ele?.title}
+                                  {` : `}
+                                  <span class="text-xs md:text-base leading-snug text-shade-2 text-opacity-100">
+                                    {ele?.discription}
+                                  </span>
+                                </p>
+                              );
+                            })}
+                          </div>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline text-shade-1">
+                          <div class="order-1 w-5/12"></div>
+                          <div class="order-1 w-5/12 px-1 py-4 text-right">
+                            <p class="mb-2 text-sm text-accent-2">
+                              {element.startDate}
+                              {`~`}
+                              {element?.endDate}
+                            </p>
+                            <h4 class="mb-3 font-bold text-md md:text-2xl">
+                              {element?.company}
+                            </h4>
+                            <h4 class="mb-1 italic text-xs md:text-2xl">
+                              {element?.role}
+                            </h4>
+                            {element?.project.map((ele) => {
+                              return (
+                                <p class="text-xs md:text-base leading-snug text-shade-1 text-opacity-100">
+                                  {ele?.title}
+                                  {` : `}
+                                  <span class="text-xs md:text-base leading-snug text-shade-2 text-opacity-100">
+                                    {ele?.discription}
+                                  </span>
+                                </p>
+                              );
+                            })}
+                          </div>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+                {/* <img class="mx-auto -mt-36 md:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
