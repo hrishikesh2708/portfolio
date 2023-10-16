a25f3c393e237bddcc455e2097fb0f53c1816f7b
Hrishikesh2708 <hrishith27@gmail.com>
2023-10-16 03:44:46 -0700

    added framer motion and updated navbar

---
+++ src/components/Logo.js
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import React from "react";
+// const MotionLink = motion(a);
+function Logo() {
+  return (
+    <div class="flex items-center justify-center mt-2">
+      <motion.a
+        href="/"
+        class=" w-16 h-16 bg-accent-3 text-shade-1 flex items-center justify-center rounded-full text-2xl font-bold"
+        whileHover={{
+          backgroundColor: [
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "#121212",
+          ],
+        }}
+        transition={{ duration: 1, repeat: Infinity }}
+      >
+        HT
+      </motion.a>
+    </div>
+  );
+}
+
+export default Logo;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,127 +1,74 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-const content = [
+import Logo from "./Logo";
+const contentl = [
   {
     label: "About Me",
     path: "#aboutMe",
+    class: "mr-4",
   },
   {
     label: "Education",
     path: "#education",
+    class: "mx-4",
   },
+  // {
+  //   label: "Contact",
+  //   path: "#contact",
+  //   class: "ml-4",
+  // },
+];
+const contentr = [
   {
     label: "Experience",
     path: "#experience",
+    class: "mx-4",
   },
   {
     label: "Project",
     path: "#project",
-  },
-  {
-    label: "Contact",
-    path: "#contact",
+    class: "mx-4",
   },
 ];
-
+const Customlink = ({ href, title, className = "" }) => {
+  return (
+    <a href={href} class={`${className} relative group`}>
+      {title}
+      <span class=" h-[0.1px] inline-block w-full bg-shade-1 left-0 -bottom-0.5 absolute group-hover:w-full transition-[width] ease duration-300">
+        &nbsp;
+      </span>
+    </a>
+  );
+};
 function Navbar() {
   return (
-    <nav class="">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span class="self-center text-xl font-semibold whitespace-nowrap text-shade-1">
-            Hrishikesh Thakur
-          </span>
-        </a>
-
-        <div class="text-center">
-          <button
-            class="text-shade-1 bg-shade-5 hover:bg-shade-4 focus:ring-2 focus:ring-shade-1 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
-            type="button"
-            data-drawer-target="drawer-right-example"
-            data-drawer-show="drawer-right-example"
-            data-drawer-placement="right"
-            aria-controls="drawer-right-example"
-          >
-            <GiHamburgerMenu />
-          </button>
-        </div>
-        <div
-          id="drawer-right-example"
-          class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
-          tabindex="-1"
-          aria-labelledby="drawer-right-label"
-        >
-          <h5
-            id="drawer-right-label"
-            class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
-          >
-            <svg
-              class="w-4 h-4 mr-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            Right drawer
-          </h5>
-          <button
-            type="button"
-            data-drawer-hide="drawer-right-example"
-            aria-controls="drawer-right-example"
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 right-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-            <span class="sr-only">Close menu</span>
-          </button>
-          <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">
-            Supercharge your hiring by taking advantage of our{" "}
-            <a
-              href="#"
-              class="text-blue-600 underline font-medium dark:text-blue-500 hover:no-underline"
-            >
-              limited-time sale
-            </a>{" "}
-            for Flowbite Docs + Job Board. Unlimited access to over 190K
-            top-ranked candidates and the #1 design job board.
-          </p>
-        </div>
-        {content.map((element) => {
+    <header class="w-full px-32 py-8 font-medium flex items-center justify-between text-shade-1">
+      <div class=" absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+      <nav class="">
+        {contentl.map((element) => {
           return (
-            <a
-              type="button"
+            <Customlink
               href={element.path}
-              class="stroke-2 bg-shade-5 hover:bg-shade-4 focus:ring-2 focus:outline-none focus:ring-shade-2 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center"
-            >
-              {/* {element.icon} */}
-              <span class="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-                {element.label}
-              </span>
-            </a>
+              title={element.label}
+              className={`${element.class}`}
+            />
           );
         })}
-      </div>
-    </nav>
+      </nav>
+      <nav class="">
+        {contentr.map((element) => {
+          return (
+            <Customlink
+              href={element.path}
+              title={element.label}
+              className={`${element.class}`}
+            />
+          );
+        })}
+      </nav>
+    </header>
   );
 }
 
