b5a93fdfeba2204f0477c2dae4fee8f73623b66c
Hrishikesh2708 <hrishith27@gmail.com>
2023-10-16 18:17:26 -0700

    fixed deployement pipeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,8 +58,8 @@ module.exports = {
           "right: 50%; border: 2px solid #FFC100; border-radius: 1%;",
       },
       backgroundImage: {
-        usc: "url('../assets/usc.webp')",
-        srm: "url('../assets/srm.jpeg')",
+        usc: "url('./assets/usc.webp')",
+        srm: "url('./assets/srm.jpeg')",
       },
     },
   },
